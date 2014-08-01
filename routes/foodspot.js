e2c2c61e690341a446a6f744684955f1c424e0f5
Kha Nguyen <nlhkha@gmail.com>
2014-08-01 20:45:01 +0700

    Update foodspot json parsing

--- routes/foodspot.js
+++ routes/foodspot.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 var foodspot_factory = require('../FoodSpottingApi/foodspot_factory');
 
-router.get('/:course', function(req, res) {
-
-	foodspot_factory.search(req.params.course, function(foodspotJSON) {
+router.get('/:food', function(req, res) {
+	foodspot_factory.search(req.params.food, function(foodspotJSON) {
+		foodspotJSON.forEach(function(item, index) {
+			console.log(item);
+		});
 		res.json(foodspotJSON);
 	});
 });
