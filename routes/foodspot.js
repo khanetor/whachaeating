79ce83373e36a523cf3560a870ad1df0118ea31d
Kha Nguyen <nlhkha@gmail.com>
2014-08-02 08:46:19 +0700

    Search food by tag

--- routes/foodspot.js
+++ routes/foodspot.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Food = require('../models/food');
 
 var foodspot_factory = require('../FoodSpottingApi/foodspot_factory');
 
@@ -9,17 +10,50 @@ router.get('/', function(req, res) {
   });
 });
 
+router.get('/tag/:tag/:lat/:long', function(req, res) {
+	// Translate from tag to food
+	// Get all foods with this tag
+	var tag = req.params.tag;
+	tagToFood(tag, function(err, foods) {
+		if (!err) {
+			if (foods.length > 0) {
+				var food_bag = [];
+				foods.forEach(function(food, index) {
+					foodspot_factory.search(food.food_name, req.params.lat, req.params.long, function(foodspotJSON) {
+						food_bag.concat(foodspotJSON);
+					});
+				});
+				res.json(food_bag);
+			} else {
+				res.json(500,  { message : 'There is no food.'});
+			}
+		} else {
+			res.json(500, { message : 'Something is wrong.'});
+		}
+	});
+});
 
-router.get('/:food', function(req, res) {
+
+router.get('/food/:food', function(req, res) {
 	foodspot_factory.search(req.params.food, '10.7721885', '106.65396659999999', function(foodspotJSON) {
 		res.json(foodspotJSON);
 	});
 });
 
-router.get('/:food/:lat/:long', function(req, res) {
+router.get('/food/:food/:lat/:long', function(req, res) {
 	foodspot_factory.search(req.params.food, req.params.lat, req.params.long, function(foodspotJSON) {
 		res.json(foodspotJSON);
 	});
 });
+
+function tagToFood(tag, callback) {
+	Food.find({ tags : tag }, function(err, foods) {
+		if (!err) {
+			callback(null, foods);
+		} else {
+			callback(err);
+		}
+	})
+};
 
 module.exports = router;
